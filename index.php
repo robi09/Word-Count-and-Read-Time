3fa5f137d0b0e5758d71b66408a1951e0be4eace
Hapiuc Robert <robert@9pixels.co>
2016-01-24 20:28:07 +0200

    basics and gitignore

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Word Count and Read Time
+* Plugin URI: http://hapiucrobert.ro
+* Description: Show word count and reading time in front-end with a nice and clean loading bar. 
+* Version: 1.0
+* Author: Hapiuc Robert
+* Author URI: http://hapiucrobert.ro
+* License: GPL
+*/
